4b24f9185e602c93fa2a774ccb97d7bdbdcd7708
Abhi-vish <iamabhishekvishwakarma7@gmail.com>
2024-04-19 12:03:49 +0530

    Made Changes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CMDTool',
+    name='CMDHelper',
     version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'cmdtool = cmdtool.cmd_tool:main'
+            'cmdhelper = cmdtool.cmd_tool:main'
         ]
     },
     install_requires=[
