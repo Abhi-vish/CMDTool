a8ccce71a15bc11af291452ebca27d2d7c687c81
Abhi-vish <iamabhishekvishwakarma7@gmail.com>
2024-04-20 11:58:59 +0530

    Changes Made in Ps1

--- Ps1Tool/powershellTool.ps1
+++ Ps1Tool/powershellTool.ps1
@@ -1,7 +1,22 @@
 param (
-    [string]$FileName
+    [string]$FileUrl
 )
 
-$content = Get-Content $FileName
+# Download the file content from the URL
+$content = Invoke-WebRequest -Uri $FileUrl
+
+# Extract the filename from the URL
+$fileName = [System.IO.Path]::GetFileName($FileUrl)
+
+# Save the downloaded content to a temporary file
+$tempFilePath = Join-Path -Path $env:TEMP -ChildPath $fileName
+$content.Content | Out-File -FilePath $tempFilePath -Encoding utf8
+
+# Copy the content of the downloaded file to the clipboard
+$content = Get-Content $tempFilePath
 $content | Set-Clipboard
-Write-Output "Content of $FileName copied to clipboard."
+
+# Remove the temporary file
+Remove-Item -Path $tempFilePath
+
+Write-Output "Content from $FileUrl copied to clipboard."
