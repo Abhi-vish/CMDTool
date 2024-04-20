416a67155aaddefa60601cdc03cf8ca067ccb27b
Abhi-vish <iamabhishekvishwakarma7@gmail.com>
2024-04-20 12:09:51 +0530

    Changes Made

--- Ps1Tool/Ps1helper.ps1
+++ Ps1Tool/Ps1helper.ps1
@@ -1,12 +1,15 @@
 param (
-    [string]$FileUrl
+    [string]$FileName
 )
 
+# Construct the URL of the raw file using the repository name and file name
+$url = "https://github.com/Abhi-vish/CMDTool/raw/main/Ps1Tool/Content/$FileName"
+
 # Download the file content from the URL
-$content = Invoke-WebRequest -Uri $FileUrl
+$content = Invoke-WebRequest -Uri $url
 
 # Extract the filename from the URL
-$fileName = [System.IO.Path]::GetFileName($FileUrl)
+$fileName = [System.IO.Path]::GetFileName($url)
 
 # Save the downloaded content to a temporary file
 $tempFilePath = Join-Path -Path $env:TEMP -ChildPath $fileName
@@ -19,4 +22,4 @@ $content | Set-Clipboard
 # Remove the temporary file
 Remove-Item -Path $tempFilePath
 
-Write-Output "Content from $FileUrl copied to clipboard."
+Write-Output "Content from $FileName in repository $RepoName copied to clipboard."
